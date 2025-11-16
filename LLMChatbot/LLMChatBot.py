4ca5675cc1c13570e5960d87c77c737249d5e458
gowtham <gowthamm.5127@gmail.com>
2025-11-16 14:19:09 +0530

    Added LLM API Request

--- LLMChatbot/LLMChatBot.py
+++ LLMChatbot/LLMChatBot.py
@@ -79,7 +79,6 @@ with st.sidebar:
             "openai/gpt-oss-20b",
             "llama-3.1-8b-instant",
             "llama-3.3-70b-versatile",
-            "meta-llama/llama-4-maverick-17b-128e-instruct",
             "meta-llama/llama-4-scout-17b-16e-instruct",
         ]
     )
